a012a9df38637fe76914c5a13b662bc727d61dd1
Nick Douma <chaos@nekoconeko.nl>
2012-06-09 16:26:43 +0200

    Restructure color support

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -7,42 +7,25 @@ if [ -z "$TERM" ]; then
 	return;
 fi
 
-# Check for color support and set colors
-if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-    RED=$(tput setaf 1)
-    GREEN=$(tput setaf 2)
-    CYAN=$(tput setaf 6)
-    WHITE=$(tput setaf 7)
-    NORMAL=$(tput sgr0)
-else
-    RED=
-    GREEN=
-    CYAN=
-    WHITE=
-    NORMAL=
-fi
-
 ## set variable identifying the chroot you work in (used in the prompt below)
 if [ -z "$debian_chroot" ] && [ -r /etc/debian_chroot ]; then
     debian_chroot=$(cat /etc/debian_chroot)
 fi
 
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color) color_prompt=yes;;
-esac
+# Set to yes to enable colors in the prompt
+enable_color_prompt=yes
 
-# uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of commands, not on the prompt
-force_color_prompt=yes
-
-if [ -n "$force_color_prompt" ]; then
+if [ -n "$enable_color_prompt" ]; then
     if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
         # We have color support; assume it's compliant with Ecma-48
         # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
         # a case would tend to support setf rather than setaf.)
         color_prompt=yes
+		RED=$(tput setaf 1)
+		GREEN=$(tput setaf 2)
+		CYAN=$(tput setaf 6)
+		WHITE=$(tput setaf 7)
+		NORMAL=$(tput sgr0)
     else
         color_prompt=
     fi
@@ -66,12 +49,10 @@ GIT_PS1_SHOWUPSTREAM="auto verbose"
 if [ "$color_prompt" = yes ]; then
 		PS1='\[${NORMAL}\]${debian_chroot:+($debian_chroot)}\u@\h\[${CYAN}\] \w \[${RED}\]$(__git_ps1 "git::%s")\[${GREEN}\]$(parse_svn_branch) \[${WHITE}\]$\[${NORMAL}\] '
 else
-        PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-		echo
+		PS1='${debian_chroot:+($debian_chroot)}\u@\h \w $(__git_ps1 "git::%s")$(parse_svn_branch) $ '
 fi
-unset color_prompt force_color_prompt
 
-# If this is an xterm set the title to user@host:dir
+# If this is an xterm set the title as well
 case "$TERM" in
 xterm*|rxvt*)
     PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w \$(__git_ps1 'git::%s')\$(parse_svn_branch)  \a\]$PS1"
@@ -79,3 +60,4 @@ xterm*|rxvt*)
 *)
     ;;
 esac
+unset color_prompt enable_color_prompt
